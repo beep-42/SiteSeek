410e17eb991eef66e1497dde0af6cfc9a69a2035
Jakub Telčer <jakubt@cuni.cz>
2025-03-31 16:06:54 +0200

    Add json support for the results table

--- src/cli.py
+++ src/cli.py
@@ -1,8 +1,23 @@
 import click
+import numpy
+
 from Database import Database, Result
 import json
 from sys import stdout
 from timeit import default_timer as timer
+
+
+def convert_to_json(results: list[Result]) -> str:
+    """Converts list of Results to a dict of dicts (index to object) and subsequently serializes it using json.dumps."""
+
+    obj = {i: r.__dict__ for i,r in enumerate(results)}
+    for one in obj:
+        single_result = obj[one]
+        for field in single_result:
+            if isinstance(single_result[field], numpy.ndarray):
+                single_result[field] = single_result[field].tolist()
+
+    return json.dumps(obj)
 
 @click.group()
 def cli():
@@ -39,11 +54,13 @@ def build(directory, output, threads, compress):
 @click.option('-kt', '--kmer-threshold', default=11, type=click.FloatRange(0, 20), help='The minimum required kmer similarity')
 @click.option('--no-clustering', is_flag=True, show_default=True, default=False, help='Disables the clustering and relies solely on the RANSAC algorithm.')
 @click.option('--ransac-min', default=15, show_default=True, type=click.IntRange(1, 1500), help='The minimum successful expansions to terminate the RANSAC algorithm.')
+@click.option('--no-refine', default=False, is_flag=True, type=click.BOOL, help='Skip ICP refinement.')
 @click.option('-o', '--output', help='Output file for the results', required=False, type=click.File(mode='w'),
               default=stdout)
 @click.option('--compressed', default=False, is_flag=True, help='Uncompress the database file.')
+@click.option('--output-json', default=False, is_flag=True, help='Output the results as JSON.')
 # @click.option('--format', help='Output format (JSON or CSV)', required=False)
-def search(database, query, site, likelihood_ratio, kmer_threshold, no_clustering, ransac_min, output, compressed):
+def search(database, query, site, likelihood_ratio, kmer_threshold, no_clustering, ransac_min, no_refine, output, compressed, output_json):
     """
     Searches the given database and returns JSON output.
 
@@ -67,12 +84,16 @@ def search(database, query, site, likelihood_ratio, kmer_threshold, no_clusterin
     click.echo(click.style('Running the search...', fg='green', bold=True), err=True)
     try:
         t = timer()
-        results = db.search(query, site=site, k_mer_similarity_threshold=kmer_threshold, lr=likelihood_ratio, skip_clustering=no_clustering, ransac_min=ransac_min)
+        results = db.search(query, site=site, k_mer_similarity_threshold=kmer_threshold, lr=likelihood_ratio,
+                            skip_clustering=no_clustering, skip_icp=no_refine, ransac_min=ransac_min)
         click.echo(click.style(f'Done in {timer() - t :.{2}f} seconds. Found {len(results)} results.', fg='cyan', bold=True), err=True)
 
         # output.write(json.dumps(results))
-        output.write(Result.get_header())
-        output.write(''.join([str(x) for x in sorted(results, key=lambda x: x.score, reverse=True)]))
+        if output_json:
+            output.write(convert_to_json(results))
+        else:
+            output.write(Result.get_header())
+            output.write(''.join([str(x) for x in sorted(results, key=lambda x: x.score, reverse=True)]))
 
     except Exception as e:
         click.echo(click.style(f"Failed! Exception: {e}", fg='red', bold=True))
